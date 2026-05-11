86fedf1a73afea6781f67e9f27bace2c0ebdad61
Robson496 <robsondiascleiton06@gmail.com>
2026-05-11 15:32:19 -0300

    Update DispositivoEletrico.java

--- DispositivoEletrico.java
+++ DispositivoEletrico.java
@@ -1,29 +1,50 @@
-package energia;
-
-public class DispositivoEletrico {
-
+abstract class DispositivoEletrico {
     protected String nome;
     protected double potenciaWatts;
-    protected double horasUso;
+    protected double horasUsoPorDia;
 
-    public DispositivoEletrico(String nome, double potenciaWatts) {
+    public DispositivoEletrico(String nome, double potenciaWatts, double horasUsoPorDia) {
         this.nome = nome;
         this.potenciaWatts = potenciaWatts;
-        this.horasUso = 0;
+        this.horasUsoPorDia = horasUsoPorDia;
     }
 
-    public void setHorasUso(double horasUso) {
-        this.horasUso = horasUso;
+    public double calcularConsumoDiario() {
+        return (potenciaWatts * horasUsoPorDia) / 1000; // kWh
     }
 
-    public double calcularConsumo() {
-        return (potenciaWatts * horasUso) / 1000;
+    public abstract String getTipo();
+}
+
+class Lampada extends DispositivoEletrico {
+    public Lampada(String nome, double potencia, double horas) {
+        super(nome, potencia, horas);
     }
 
-    public void exibirInformacoes() {
-        System.out.println("Dispositivo: " + nome);
-        System.out.println("Potência: " + potenciaWatts + "W");
-        System.out.println("Horas de uso: " + horasUso);
-        System.out.println("Consumo: " + calcularConsumo() + " kWh");
+    @Override
+    public String getTipo() {
+        return "Iluminação";
+    }
+}
+
+class ArCondicionado extends DispositivoEletrico {
+    public ArCondicionado(String nome, double potencia, double horas) {
+        super(nome, potencia, horas);
+    }
+
+    @Override
+    public String getTipo() {
+        return "Climatização";
+    }
+}
+
+class Televisao extends DispositivoEletrico {
+    public Televisao(String nome, double potencia, double horas) {
+        super(nome, potencia, horas);
+    }
+
+    @Override
+    public String getTipo() {
+        return "Entretenimento (linha marrom)";
     }
 }
