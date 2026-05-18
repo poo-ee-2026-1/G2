fc375ac5a91cfe560883810f5696ee3587f211fe
Robson496 <robsondiascleiton06@gmail.com>
2026-05-18 13:47:51 -0300

    Create LeituraConsumo.java

---
+++ LeituraConsumo.java
@@ -0,0 +1,13 @@
+package sistema;
+
+public class LeituraConsumo {
+    private double consumo;
+
+    public LeituraConsumo(double consumo) {
+        this.consumo = consumo;
+    }
+
+    public double getConsumo() {
+        return consumo;
+    }
+}
